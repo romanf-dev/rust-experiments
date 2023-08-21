6d2645a72dc693c7d4aa5fbe518f9774edc4daf3
Roman <67749110+romanf-dev@users.noreply.github.com>
2023-08-22 01:50:32 +0300

    Tabs fixes

--- linked_list/src/main.rs
+++ linked_list/src/main.rs
@@ -15,25 +15,25 @@ impl<T> Node<T> {
     
     fn set_next(&mut self, new_next: *mut Node<T>) {
         if let Some((prev, _)) = self.links {
-	        self.links = Some((prev, new_next));
+            self.links = Some((prev, new_next));
         }
     }
 	
     fn set_prev(&mut self, new_prev: *mut Node<T>) {
-		if let Some((_, next)) = self.links {
-			self.links = Some((new_prev, next));
-		}
-	}
+        if let Some((_, next)) = self.links {
+            self.links = Some((new_prev, next));
+        }
+    }
 	
-	fn to_obj(&mut self) -> &mut T {
+    fn to_obj(&mut self) -> &mut T {
         let ptr = self.payload.take().unwrap();
         unsafe { &mut *ptr }
-	}
+    }
 }
 
 impl<T> Drop for Node<T> {
     fn drop(&mut self) {
-		println!("node drop");
+        println!("node drop");
         assert!(self.links.is_none());
     }
 }
@@ -56,27 +56,26 @@ impl<'a, T: Linkable> List<'a, T> {
     }
 
     fn init(&mut self) {
-		let this = (&mut self.root) as *mut Node<T>;
+        let this = (&mut self.root) as *mut Node<T>;
         self.root.links = Some((this, this));
     }
 
     fn get_head(&mut self) -> Option<&mut Node<T>> {
-		match self.root.links { 
-			Some((_, next)) => 
-				if next != &mut self.root { 
-					unsafe { Some(&mut *next) } 
-				} else { 
-					None 
-				},
-			_ => None
-		}		
+        match self.root.links { 
+            Some((_, next)) => 
+                if next != &mut self.root { 
+                    unsafe { Some(&mut *next) } 
+                } else { 
+                    None 
+                },
+            _ => None
+        }		
     }
     
     fn push<'b>(&mut self, object: &'b mut T) {
         let ptr: *mut T = object;
         let node = object.to_links();
         let (prev, next) = self.root.links.take().unwrap();
-        
         node.links = Some((prev, &mut self.root));
         self.root.links = Some((node, next));
         unsafe { (*prev).set_next(node); }
@@ -84,23 +83,23 @@ impl<'a, T: Linkable> List<'a, T> {
     }
 
     fn pop(&mut self) -> Option<&mut T> {
-		if let Some(node) = self.get_head() {
-			let (prev, next) = node.links.take().unwrap();
+        if let Some(node) = self.get_head() {
+            let (prev, next) = node.links.take().unwrap();
 
-			unsafe {
-				(*prev).set_next(next);
-				(*next).set_prev(prev);
-			}
+            unsafe {
+                (*prev).set_next(next);
+                (*next).set_prev(prev);
+            }
 			
-			return Some(node.to_obj());
-		}
-		None
+            return Some(node.to_obj());
+        }
+        None
     }
 }
 
 impl<'a, T: Linkable> Drop for List<'a, T> {
     fn drop(&mut self) {
-		println!("list drop");
+        println!("list drop");
         assert!(self.get_head().is_none());
         self.root.links = None;
     }
