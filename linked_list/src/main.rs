51815a19604ad6238bc1b9ae64c146c77a8cbf38
romanf-dev <>
2023-10-06 02:31:45 +0300

    cell for list internal members

--- linked_list/src/main.rs
+++ linked_list/src/main.rs
@@ -1,44 +1,39 @@
 use core::marker::PhantomData;
+use core::cell::Cell;
 
 struct Node<T> {
-    links: Option<(*mut Node<T>, *mut Node<T>)>,
-    payload: Option<*mut T>,
+    links: Cell<Option<(*const Node<T>, *const Node<T>)>>,
+    payload: Cell<Option<*mut T>>,
 }
 
 impl<T> Node<T> {
     const fn new() -> Node<T> {
         Node {
-            links: None,
-            payload: None,
+            links: Cell::new(None),
+            payload: Cell::new(None),
         }
     }
     
-    fn set_next(&mut self, new_next: *mut Node<T>) {
-        if let Some((prev, _)) = self.links {
-            self.links = Some((prev, new_next));
+    fn set_next(&self, new_next: *const Node<T>) {
+        if let Some((prev, _)) = self.links.take() {
+            self.links.set(Some((prev, new_next)));
         }
     }
     
-    fn set_prev(&mut self, new_prev: *mut Node<T>) {
-        if let Some((_, next)) = self.links {
-            self.links = Some((new_prev, next));
+    fn set_prev(&self, new_prev: *const Node<T>) {
+        if let Some((_, next)) = self.links.take() {
+            self.links.set(Some((new_prev, next)));
         }
     }
     
-    fn to_obj(&mut self) -> &mut T {
+    fn to_obj(&self) -> &mut T {
         let ptr = self.payload.take().unwrap();
         unsafe { &mut *ptr }
     }
 }
 
-impl<T> Drop for Node<T> {
-    fn drop(&mut self) {
-        assert!(self.links.is_none());
-    }
-}
-
 trait Linkable: Sized {
-    fn to_links(&mut self) -> &mut Node<Self>;
+    fn to_links(&self) -> &Node<Self>;
 }
 
 struct List<'a, T: Linkable> {
@@ -54,34 +49,34 @@ impl<'a, T: Linkable> List<'a, T> {
         }
     }
 
-    fn init(&mut self) {
-        let this = (&mut self.root) as *mut Node<T>;
-        self.root.links = Some((this, this));
+    fn init(&self) {
+        let this = (&self.root) as *const Node<T>;
+        self.root.links.set(Some((this, this)));
     }
 
-    fn get_head(&mut self) -> Option<&mut Node<T>> {
-        match self.root.links { 
+    fn get_head(&self) -> Option<&Node<T>> {
+        match self.root.links.get() { 
             Some((_, next)) => 
-                if next != &mut self.root { 
-                    unsafe { Some(&mut *next) } 
+                if next != &self.root { 
+                    unsafe { Some(&*next) } 
                 } else { 
                     None 
                 },
             _ => None
-        }       
+        }
     }
     
-    fn push<'b>(&mut self, object: &'b mut T) {
+    fn push<'b>(&self, object: &'b mut T) {
         let ptr: *mut T = object;
         let node = object.to_links();
         let (prev, next) = self.root.links.take().unwrap();
-        node.links = Some((prev, &mut self.root));
-        self.root.links = Some((node, next));
+        node.links.set(Some((prev, &self.root as *const Node<T>)));
+        self.root.links.set(Some((node, next)));
         unsafe { (*prev).set_next(node); }
-        node.payload = Some(ptr);
+        node.payload.set(Some(ptr));
     }
 
-    fn pop(&mut self) -> Option<&mut T> {
+    fn pop(&self) -> Option<&mut T> {
         if let Some(node) = self.get_head() {
             let (prev, next) = node.links.take().unwrap();
 
@@ -96,31 +91,25 @@ impl<'a, T: Linkable> List<'a, T> {
     }
 }
 
-impl<'a, T: Linkable> Drop for List<'a, T> {
-    fn drop(&mut self) {
-        assert!(self.get_head().is_none());
-        self.root.links = None;
-    }
-}
-
 struct Item {
     foo: usize,
     linkage: Node<Self>
 }
 
 impl Linkable for Item {
-    fn to_links(&mut self) -> &mut Node<Self> {
-        &mut self.linkage
+    fn to_links(&self) -> &Node<Self> {
+        &self.linkage
     }
 }
 
 static mut LIST: List<Item> = List::<Item>::new();
 
 fn main() {
-    let mut items: [Item; 3] = [
+    let mut items: [Item; 4] = [
         Item { foo: 1, linkage: Node::new() },
         Item { foo: 2, linkage: Node::new() },
         Item { foo: 3, linkage: Node::new() },
+        Item { foo: 4, linkage: Node::new() },
     ];
 
     unsafe {
@@ -128,6 +117,7 @@ fn main() {
         LIST.push(&mut items[0]);
         LIST.push(&mut items[1]);
         LIST.push(&mut items[2]);
+        LIST.push(&mut items[3]);
                 
         while let Some(val) = LIST.pop() {
             println!("val = {}", val.foo);
